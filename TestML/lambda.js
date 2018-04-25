f03f78d635184a741dfe0e07e0c6b24e4013577f
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-25 15:45:01 +0530

    ML testing

---
+++ TestML/lambda.js
@@ -0,0 +1,19 @@
+let AWS = require('aws-sdk');
+const ml = new AWS.MachineLearning();
+exports.handler = function (event, context, callback) {
+	ml.predict({
+		MLModelId: 'ml-DYPsae14K89',
+		PredictEndpoint: 'https://realtime.machinelearning.us-east-1.amazonaws.com',
+		Record: {
+			age: 32.0,
+			job: services,
+			marital: divorced
+		}
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
+
+
+	// callback(null, 'Successfully executed');
+}
